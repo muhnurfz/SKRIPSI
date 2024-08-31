8b559db4f65cb83bff0569c1da7c84c9e55239ce
Muhamad Nur Fajjri Zenitandrio <125628422+muhnurfz@users.noreply.github.com>
2024-08-31 17:44:03 +0700

    "Updated text in index.php: replaced 'TESTING SELESAI GIT AUTO DEPLOY' with 'OKE SELESAI TESTING GIT 123'"

--- index.php
+++ index.php
@@ -229,7 +229,7 @@
     >
         PO.LAJU PRIMA
     </span> 
-    Anda dari sini mulai langkah pesan - bayar - berangkat
+    Anda dari sini mulai langkah pesan - bayar - berangkat TESTING SELESAI GIT AUTO DEPLOY
 </p>
 
 
