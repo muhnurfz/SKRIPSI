55e12d34550238d801f2fdd1be6db0c03c0b6414
Muhamad Nur Fajjri Zenitandrio <125628422+muhnurfz@users.noreply.github.com>
2024-09-02 15:12:11 +0700

    1

--- dashboard_pnp.php
+++ dashboard_pnp.php
@@ -257,6 +257,14 @@ $result_orders = $stmt_orders->get_result();
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script>
+      document.getElementById('toggleSidebar').addEventListener('click', function() {
+            const sidebar = document.getElementById('sidebar');
+            const content = document.getElementById('content');
+            sidebar.classList.toggle('collapsed');
+            content.classList.toggle('sidebar-collapsed');
+        });
+    </script>
 </body>
 </html>
 
