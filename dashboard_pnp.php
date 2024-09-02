fe6efc7332b851b9fc3b9e77c12ed0a52036e539
Muhamad Nur Fajjri Zenitandrio <125628422+muhnurfz@users.noreply.github.com>
2024-09-03 00:01:07 +0700

    1

--- dashboard_pnp.php
+++ dashboard_pnp.php
@@ -59,14 +59,11 @@ $passenger = $result_pnp->fetch_assoc();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard Penumpang</title>
     <!-- Bootstrap CSS -->
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Font Awesome for Icons -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
     <style>
       
     </style>
 </head>
 <body>
-
+<p> testing </p>
 </body>
 </html>
