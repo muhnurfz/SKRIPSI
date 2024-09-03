7e2caab9e88623a6f31c22c90f43701445bc021f
Muhamad Nur Fajjri Zenitandrio <125628422+muhnurfz@users.noreply.github.com>
2024-09-03 08:24:50 +0700

    1

--- navbar.php
+++ navbar.php
@@ -66,7 +66,7 @@ $passenger = $result_pnp->fetch_assoc();
         .profile-link {
             position: absolute;
             margin-left: auto; /* Push to the far right */
-            right: 200px; /* Margin from right */
+            right: 250px; /* Margin from right */
             top: 50%;
             transform: translateY(-50%);
         }
